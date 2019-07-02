cdb5d258f87fcefa50ed17e5ffa41cc33ce276d6
SadDone <47665715+SadDone@users.noreply.github.com>
2019-07-02 20:49:57 +0300

    Update udpserver.c

--- lab7/src/udpserver.c
+++ lab7/src/udpserver.c
@@ -1,56 +1,135 @@
-#include <arpa/inet.h>
 #include <netinet/in.h>
+
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <string.h>
+
+
+
 #include <sys/socket.h>
-#include <sys/stat.h>
+
 #include <sys/types.h>
+
 #include <unistd.h>
 
-#define SERV_PORT 20001
-#define BUFSIZE 1024
-#define SADDR struct sockaddr
-#define SLEN sizeof(struct sockaddr_in)
 
-int main() {
-  int sockfd, n;
+
+#include<signal.h>
+
+
+
+#define SERV_PORT 10050
+
+#define SERV_P 20001
+
+#define BUFSIZE 1024
+
+#define SADDR struct sockaddr
+
+#define SADDR struct sockaddr2
+
+
+
+#define SLEN sizeof(struct sockaddr)
+
+int main(void)
+{
+    int gfd, n;
+
   char mesg[BUFSIZE], ipadr[16];
-  struct sockaddr_in servaddr;
+
+  struct sockaddr_in servaddr2;
+
+  struct sockaddr_in cliaddr2;
+  
   struct sockaddr_in cliaddr;
 
-  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+
+  if ((gfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+
     perror("socket problem");
+
     exit(1);
+
   }
 
-  memset(&servaddr, 0, SLEN);
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  servaddr.sin_port = htons(SERV_PORT);
+  printf("test7");
 
-  if (bind(sockfd, (SADDR *)&servaddr, SLEN) < 0) {
-    perror("bind problem");
+
+
+  memset(&servaddr2, 0, SLEN);
+
+  servaddr2.sin_family = AF_INET;
+
+  //servaddr2.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  servaddr2.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+  servaddr2.sin_port = htons(SERV_P);
+
+
+
+  if (bind(gfd, (SADDR *)&servaddr2, SLEN) < 0) {
+
+    perror("bind problem100");
+
     exit(1);
+
   }
+
+  printf("test8");
+
   printf("SERVER starts...\n");
 
-  while (1) {
-    unsigned int len = SLEN;
+  perror("read?0");
 
-    if ((n = recvfrom(sockfd, mesg, BUFSIZE, 0, (SADDR *)&cliaddr, &len)) < 0) {
+  int upd_stop_flag=1;
+
+  perror("read?1");
+
+  while (upd_stop_flag==1) {
+
+      perror("read?2");
+
+    unsigned int len = sizeof(struct sockaddr);
+
+    perror("read?3");
+
+    if ((n = recvfrom(gfd, mesg, BUFSIZE, 0, (SADDR *)&cliaddr2, &len)) < 0) {
+
       perror("recvfrom");
+
       exit(1);
+
     }
+
+    perror("test9");
+
     mesg[n] = 0;
 
+    /*
+
     printf("REQUEST %s      FROM %s : %d\n", mesg,
-           inet_ntop(AF_INET, (void *)&cliaddr.sin_addr.s_addr, ipadr, 16),
+
+           inet_ntop(AF_INET, (void *)&cliaddr2.sin_addr.s_addr, ipadr, 16),
+
            ntohs(cliaddr.sin_port));
 
-    if (sendto(sockfd, mesg, n, 0, (SADDR *)&cliaddr, len) < 0) {
+           */
+
+perror("test9!");
+
+    if (sendto(gfd, mesg, n, 0, (SADDR *)&cliaddr2, len) < 0) {
+
       perror("sendto");
+
       exit(1);
+
+      }
+
+      perror("test10");
+
     }
-  }
 }
